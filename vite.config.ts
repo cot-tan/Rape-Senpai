d4aba2fed0cffa8c373985082020c07521fcb7f0
常用対数の底 <102527663+cot-tan@users.noreply.github.com>
2025-05-28 09:56:04 +0900

    feature: pwa化したかも

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import * as manifest from './site.webmanifest' with { type: "json" }
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react()],
+  plugins: [deno(), react(), VitePWA(manifest)],
   assetsInclude: ["png", "mp3"],
   publicDir: 'src'
 })
